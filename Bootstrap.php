d19e7ddb54d8eb75ab89753e942f88265e0d9134
Johannes <things4it@gmail.com>
2021-03-31 00:01:37 +0200

    initial commit - plugin created via jtl_plugin_bootstrapper

---
+++ Bootstrap.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+/**
+ * @package Plugin\things4it_category_image_generation
+ * @author  Johannes Wendig
+ */
+
+namespace Plugin\things4it_category_image_generation;
+
+use JTL\Plugin\Bootstrapper;
+
+/**
+ * Class Bootstrap
+ * @package Plugin\things4it_category_image_generation
+ */
+class Bootstrap extends Bootstrapper
+{
+
+}
