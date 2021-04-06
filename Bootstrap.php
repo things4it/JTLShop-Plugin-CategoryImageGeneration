68bfe80c90637222d8a3e2402b599c1b8a7be593
Johannes <things4it@gmail.com>
2021-04-06 22:40:53 +0200

    setup: cronjob which creates a category image based on the first article of a category

--- Bootstrap.php
+++ Bootstrap.php
@@ -6,6 +6,8 @@
 
 namespace Plugin\things4it_category_image_generation;
 
+use JTL\Events\Dispatcher;
+use JTL\Events\Event;
 use JTL\Plugin\Bootstrapper;
 
 /**
@@ -15,4 +17,51 @@ use JTL\Plugin\Bootstrapper;
 class Bootstrap extends Bootstrapper
 {
 
+    private const CATEGORY_IMAGE_GENERATION_CRON_JOB = 'things4it_category_image_generation_cronjob';
+
+    /**
+     * @inheritdoc
+     */
+    public function boot(Dispatcher $dispatcher): void
+    {
+        parent::boot($dispatcher);
+
+        $dispatcher->listen(Event::MAP_CRONJOB_TYPE, static function (array $args) {
+            if ($args['type'] === self::CATEGORY_IMAGE_GENERATION_CRON_JOB) {
+                $args['mapping'] = CategoryImageGenerationCronJob::class;
+            }
+        });
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function installed()
+    {
+        parent::installed();
+        $this->addCron();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function uninstalled(bool $deleteData = true)
+    {
+        parent::uninstalled($deleteData);
+        $this->getDB()->delete('tcron', 'jobType', self::CATEGORY_IMAGE_GENERATION_CRON_JOB);
+    }
+
+
+    private function addCron(): void
+    {
+        $job = new \stdClass();
+        $job->name = 'Kategorie-Bild generierung';
+        $job->jobType = self::CATEGORY_IMAGE_GENERATION_CRON_JOB;
+        $job->frequency = 24;
+        $job->startDate = 'NOW()';
+        $job->startTime = '22:30:00';
+
+        $this->getDB()->insert('tcron', $job);
+    }
 }
