f9c642dd6d93a0bddb79e47fb74509b7f304b20b
Johannes <things4it@gmail.com>
2021-05-01 23:46:00 +0200

    fix image generation to avoid php-execution-time-limit -> chunk it ...

---
+++ Migrations/Migration20210501000000.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace Plugin\things4it_category_image_generation\Migrations;
+
+use JTL\Plugin\Migration;
+use JTL\Update\IMigration;
+
+/**
+ * Class Migration20210501000000
+ * @package Plugin\things4it_category_image_generation\Migrations
+ */
+class Migration20210501000000 extends Migration implements IMigration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->execute(
+            'CREATE TABLE IF NOT EXISTS `xplugin_t4it_category_image_generation_job_queue` (                
+                `kKategorie` INT NOT NULL PRIMARY KEY                
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci'
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        if ($this->doDeleteData()) {
+            $this->execute('DROP TABLE IF EXISTS `xplugin_t4it_category_image_generation_job_queue`');
+        }
+    }
+}
