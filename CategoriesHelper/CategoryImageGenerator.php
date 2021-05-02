80aa06a4274d9f3001debdd895bd4c2d4534ea7d
Johannes <things4it@gmail.com>
2021-05-02 21:34:23 +0200

    refactoring - extract sql into daos and image-generation into separate class

---
+++ CategoriesHelper/CategoryImageGenerator.php
@@ -0,0 +1,137 @@
+<?php
+
+
+namespace Plugin\things4it_category_image_generation\CategoriesHelper;
+
+
+use Plugin\things4it_category_image_generation\Bootstrap;
+
+class CategoryImageGenerator
+{
+
+    /**
+     * @param int $categoryId
+     * @param Image[] $productImages
+     * @return string
+     */
+    public static function generateCategoryImage(int $categoryId, array $productImages): string
+    {
+        $categoryImage = self::createTransparentImage(1024, 1024);
+
+        $productImagesCount = sizeof($productImages);
+        if ($productImagesCount == 3) {
+            $imageNumber = 0;
+            foreach ($productImages as $productImage) {
+                $sourceImagePath = \PFAD_ROOT . \PFAD_MEDIA_IMAGE_STORAGE . $productImage->getCPath();
+                if (\file_exists($sourceImagePath)) {
+                    $image = self::getResizedArticleImage($sourceImagePath, 500, 500);
+                    if ($imageNumber == 0) {
+                        \imagecopy($categoryImage, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
+                    } elseif ($imageNumber == 1) {
+                        \imagecopy($categoryImage, $image, 500, 24, 0, 0, imagesx($image), imagesy($image));
+                    } else {
+                        \imagecopy($categoryImage, $image, 250, 500 + 24, 0, 0, imagesx($image), imagesy($image));
+                    }
+
+                    \imagedestroy($image);
+                }
+
+                $imageNumber++;
+            }
+        } elseif ($productImagesCount == 2) {
+            $imageNumber = 0;
+            foreach ($productImages as $productImage) {
+                $sourceImagePath = \PFAD_ROOT . \PFAD_MEDIA_IMAGE_STORAGE . $productImage->getCPath();
+                if (\file_exists($sourceImagePath)) {
+                    $image = self::getResizedArticleImage($sourceImagePath, 500, 500);
+                    if ($imageNumber == 0) {
+                        \imagecopy($categoryImage, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
+                    } else {
+                        \imagecopy($categoryImage, $image, 500, 500, 0, 0, imagesx($image), imagesy($image));
+                    }
+
+                    \imagedestroy($image);
+                }
+
+                $imageNumber++;
+            }
+        } elseif ($productImagesCount == 1) {
+            $productImage = $productImages[0];
+
+            $sourceImagePath = \PFAD_ROOT . \PFAD_MEDIA_IMAGE_STORAGE . $productImage->getCPath();
+            if (\file_exists($sourceImagePath)) {
+                $image = self::getResizedArticleImage($sourceImagePath, 1024, 1024);
+                \imagecopy($categoryImage, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
+                \imagedestroy($image);
+            }
+
+        }
+
+        $targetImageName = Bootstrap::CATEGORY_IMAGE_NAME_PREFIX . $categoryId . '.png';
+        $targetImagePath = \PFAD_ROOT . \STORAGE_CATEGORIES . $targetImageName;
+        \imagecropauto($categoryImage);
+        \imagepng($categoryImage, $targetImagePath);
+        \imagedestroy($categoryImage);
+
+        return $targetImageName;
+    }
+
+    /**
+     * @param int $width
+     * @param int $height
+     * @return false|\GdImage|resource
+     */
+    private static function createTransparentImage(int $width, int $height)
+    {
+        $image = \imagecreatetruecolor($width, $height);
+        $colorTransparent = \imagecolorallocatealpha($image, 0, 0, 0, 127);
+        \imagefill($image, 0, 0, $colorTransparent);
+        \imagealphablending($image, true);
+        \imagesavealpha($image, true);
+
+        return $image;
+    }
+
+
+    /**
+     * @param string $originalImagePath
+     * @param int $targetWidth
+     * @param int $targetHeight
+     * @return false|\GdImage|resource
+     */
+    private static function getResizedArticleImage(string $originalImagePath, int $targetWidth = 640, int $targetHeight = 640)
+    {
+        list($originalImageWidth, $originalImageHeight, $originalImageType) = \getimagesize($originalImagePath);
+        switch ($originalImageType) {
+            case \IMAGETYPE_GIF:
+                $imageOriginal = \imagecreatefromgif($originalImagePath);
+                break;
+            case \IMAGETYPE_PNG:
+                $imageOriginal = \imagecreatefrompng($originalImagePath);
+                break;
+            case \IMAGETYPE_JPEG:
+            default:
+                $imageOriginal = \imagecreatefromjpeg($originalImagePath);
+                break;
+        }
+
+        if ($originalImageWidth > $originalImageHeight) {
+            $scale = $targetWidth / $originalImageWidth;
+        } else {
+            $scale = $targetHeight / $originalImageHeight;
+        }
+
+        $newWidth = $originalImageWidth * $scale;
+        $newHeight = $originalImageHeight * $scale;
+
+        $offsetX = ($targetWidth - $newWidth) / 2;
+        $offsetY = ($targetHeight - $newHeight) / 2;
+
+        $imageResized = self::createTransparentImage($targetWidth, $targetHeight);
+        \imagecopyresampled($imageResized, $imageOriginal, $offsetX, $offsetY, 0, 0, $newWidth, $newHeight, $originalImageWidth, $originalImageHeight);
+        \imagedestroy($imageOriginal);
+
+        return $imageResized;
+    }
+
+}
--- CategoryImageGenerationCronJob.php
+++ CategoryImageGenerationCronJob.php
@@ -8,7 +8,7 @@ use JTL\Cron\QueueEntry;
 use Plugin\things4it_category_image_generation\CategoriesCronJobQueue\CategoryCronJobEntry;
 use Plugin\things4it_category_image_generation\CategoriesCronJobQueue\CategoryCronJobQueueDao;
 use Plugin\things4it_category_image_generation\CategoriesHelper\CategoryHelperDao;
-use Plugin\things4it_category_image_generation\CategoriesHelper\Image;
+use Plugin\things4it_category_image_generation\CategoriesHelper\CategoryImageGenerator;
 
 /**
  * Class CategoryImageGenerationCronJob
@@ -72,147 +72,15 @@ class CategoryImageGenerationCronJob extends Job
         $randomArticleImages = CategoryHelperDao::findRandomArticleImages($category->getKKategorie(), $this->db);
         $randomArticleImagesCount = sizeof($randomArticleImages);
         if ($randomArticleImagesCount > 0) {
-            $categoryImage = $this->generateCategoryImage($randomArticleImagesCount, $randomArticleImages);
-            $this->safeCategoryImageAndUpdateDb($category->getKKategorie(), $categoryImage);
+            $categoryImagePath = CategoryImageGenerator::generateCategoryImage($category->getKKategorie(), $randomArticleImages);
+            CategoryHelperDao::saveCategoryImage($category->getKKategorie(), $categoryImagePath, $this->db);
+
             $this->logger->debug(sprintf('Category-Image-Generation-CronJob: Image for category %s created', $category->getKKategorie()));
         } else {
             $this->logger->debug(sprintf('Category-Image-Generation-CronJob: Could not create image for category %s - no articles with images found', $category->getKKategorie()));
         }
 
         CategoryCronJobQueueDao::delete($category->getKKategorie(), $this->db);
-    }
-
-    /**
-     * @param int $randomArticleImagesCount
-     * @param Image[] $randomArticleImages
-     * @return false|\GdImage|resource
-     */
-    private function generateCategoryImage(int $randomArticleImagesCount, array $randomArticleImages)
-    {
-        $categoryImage = $this->createTransparentImage(1024, 1024);
-
-        if ($randomArticleImagesCount == 3) {
-            $imageNumber = 0;
-            foreach ($randomArticleImages as $randomArticleImage) {
-                $sourceImagePath = \PFAD_ROOT . \PFAD_MEDIA_IMAGE_STORAGE . $randomArticleImage->getCPath();
-                if (\file_exists($sourceImagePath)) {
-                    $image = $this->getResizedArticleImage($sourceImagePath, 500, 500);
-                    if ($imageNumber == 0) {
-                        \imagecopy($categoryImage, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
-                    } elseif ($imageNumber == 1) {
-                        \imagecopy($categoryImage, $image, 500, 24, 0, 0, imagesx($image), imagesy($image));
-                    } else {
-                        \imagecopy($categoryImage, $image, 250, 500 + 24, 0, 0, imagesx($image), imagesy($image));
-                    }
-
-                    \imagedestroy($image);
-                }
-
-                $imageNumber++;
-            }
-        } elseif ($randomArticleImagesCount == 2) {
-            $imageNumber = 0;
-            foreach ($randomArticleImages as $randomArticleImage) {
-                $sourceImagePath = \PFAD_ROOT . \PFAD_MEDIA_IMAGE_STORAGE . $randomArticleImage->getCPath();
-                if (\file_exists($sourceImagePath)) {
-                    $image = $this->getResizedArticleImage($sourceImagePath, 500, 500);
-                    if ($imageNumber == 0) {
-                        \imagecopy($categoryImage, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
-                    } else {
-                        \imagecopy($categoryImage, $image, 500, 500, 0, 0, imagesx($image), imagesy($image));
-                    }
-
-                    \imagedestroy($image);
-                }
-
-                $imageNumber++;
-            }
-        } elseif ($randomArticleImagesCount == 1) {
-            $randomArticleImage = $randomArticleImages[0];
-
-            $sourceImagePath = \PFAD_ROOT . \PFAD_MEDIA_IMAGE_STORAGE . $randomArticleImage->getCPath();
-            if (\file_exists($sourceImagePath)) {
-                $image = $this->getResizedArticleImage($sourceImagePath, 1024, 1024);
-                \imagecopy($categoryImage, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
-                \imagedestroy($image);
-            }
-
-        }
-
-        return $categoryImage;
-    }
-
-    /**
-     * @param int $width
-     * @param int $height
-     * @return false|\GdImage|resource
-     */
-    private function createTransparentImage(int $width, int $height)
-    {
-        $image = \imagecreatetruecolor($width, $height);
-        $colorTransparent = \imagecolorallocatealpha($image, 0, 0, 0, 127);
-        \imagefill($image, 0, 0, $colorTransparent);
-        \imagealphablending($image, true);
-        \imagesavealpha($image, true);
-
-        return $image;
-    }
-
-
-    /**
-     * @param string $originalImagePath
-     * @param int $targetWidth
-     * @param int $targetHeight
-     * @return false|\GdImage|resource
-     */
-    private function getResizedArticleImage(string $originalImagePath, int $targetWidth = 640, int $targetHeight = 640)
-    {
-        list($originalImageWidth, $originalImageHeight, $originalImageType) = \getimagesize($originalImagePath);
-        switch ($originalImageType) {
-            case \IMAGETYPE_GIF:
-                $imageOriginal = \imagecreatefromgif($originalImagePath);
-                break;
-            case \IMAGETYPE_PNG:
-                $imageOriginal = \imagecreatefrompng($originalImagePath);
-                break;
-            case \IMAGETYPE_JPEG:
-            default:
-                $imageOriginal = \imagecreatefromjpeg($originalImagePath);
-                break;
-        }
-
-        if ($originalImageWidth > $originalImageHeight) {
-            $scale = $targetWidth / $originalImageWidth;
-        } else {
-            $scale = $targetHeight / $originalImageHeight;
-        }
-
-        $newWidth = $originalImageWidth * $scale;
-        $newHeight = $originalImageHeight * $scale;
-
-        $offsetX = ($targetWidth - $newWidth) / 2;
-        $offsetY = ($targetHeight - $newHeight) / 2;
-
-        $imageResized = $this->createTransparentImage($targetWidth, $targetHeight);
-        \imagecopyresampled($imageResized, $imageOriginal, $offsetX, $offsetY, 0, 0, $newWidth, $newHeight, $originalImageWidth, $originalImageHeight);
-        \imagedestroy($imageOriginal);
-
-        return $imageResized;
-    }
-
-    /**
-     * @param int $categoryId
-     * @param object $categoryImage
-     */
-    private function safeCategoryImageAndUpdateDb(int $categoryId, $categoryImage): void
-    {
-        $targetImageName = Bootstrap::CATEGORY_IMAGE_NAME_PREFIX . $categoryId . '.png';
-        $targetImagePath = \PFAD_ROOT . \STORAGE_CATEGORIES . $targetImageName;
-        \imagecropauto($categoryImage);
-        \imagepng($categoryImage, $targetImagePath);
-        \imagedestroy($categoryImage);
-
-        CategoryHelperDao::saveCategoryImage($categoryId, $targetImagePath, $this->db);
     }
 
 }
