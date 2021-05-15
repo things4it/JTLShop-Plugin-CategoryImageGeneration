84db818450c58fb9ec424d916c67792e3e37fdb5
Johannes <things4it@gmail.com>
2021-05-16 00:43:29 +0200

    refactor to use a service to generate the image to remove duplicate code
    * deletion of the already created images removed from re-generation via admin-menu - it was never be required

--- src/db/entity/CategoryCronJobEntry.php
+++ src/db/entity/CategoryCronJobEntry.php
@@ -2,26 +2,7 @@
 
 namespace Plugin\t4it_category_image_generation\src\db\entity;
 
-class CategoryCronJobEntry
+class CategoryCronJobEntry extends Category
 {
-
-    private int $kKategorie;
-
-    /**
-     * @return int
-     */
-    public function getKKategorie(): int
-    {
-        return $this->kKategorie;
-    }
-
-    /**
-     * @param int $kKategorie
-     */
-    public function setKKategorie(int $kKategorie): void
-    {
-        $this->kKategorie = $kKategorie;
-    }
-
 
 }
---
+++ src/service/CategoryImageGenerationService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Plugin\t4it_category_image_generation\src\service;
+
+use JTL\DB\DbInterface;
+use Plugin\t4it_category_image_generation\src\db\dao\CategoryHelperDao;
+use Plugin\t4it_category_image_generation\src\utils\CategoryImageGenerator;
+
+class CategoryImageGenerationService
+{
+
+    /**
+     * @param int $categoryId
+     * @param DbInterface $db
+     * @throws \Exception
+     */
+    public static function generateCategoryImage(int $categoryId, DbInterface $db)
+    {
+        $randomArticleImages = CategoryHelperDao::findRandomArticleImages($categoryId, $db);
+        $randomArticleImagesCount = sizeof($randomArticleImages);
+        if ($randomArticleImagesCount > 0) {
+            $categoryImagePath = CategoryImageGenerator::generateCategoryImage($categoryId, $randomArticleImages);
+            CategoryHelperDao::saveCategoryImage($categoryId, $categoryImagePath, $db);
+            \JTL\Media\Image\Category::clearCache($categoryId);
+        } else {
+            throw new \Exception(sprintf('No articles with images found for category %s', $categoryId));
+        }
+
+    }
+
+}
