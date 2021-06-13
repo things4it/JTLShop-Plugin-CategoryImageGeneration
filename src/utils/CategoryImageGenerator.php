39fe5762515d2dcfab3a1f6e19bc911ef62f2398
Johannes <things4it@gmail.com>
2021-06-13 23:24:35 +0200

    refactor product image copy to category image in CategoryImageGenerator.php into a separate method and changed to create all temp product images before copy it dependent on the count of the images

--- src/utils/CategoryImageGenerator.php
+++ src/utils/CategoryImageGenerator.php
@@ -5,6 +5,7 @@ namespace Plugin\t4it_category_image_generation\src\utils;
 
 
 use Plugin\t4it_category_image_generation\src\db\entity\Image;
+use Plugin\t4it_category_image_generation\src\service\ProductImagesPlacementService;
 
 class CategoryImageGenerator
 {
@@ -23,56 +24,9 @@ class CategoryImageGenerator
      */
     public static function generateCategoryImage(int $categoryId, array $productImages): string
     {
-        $categoryImage = self::createTransparentImage(1024, 1024);
+        $categoryImage = ImageUtils::createTransparentImage(1024, 1024);
 
-        $productImagesCount = sizeof($productImages);
-        if ($productImagesCount == 3) {
-            $imageNumber = 0;
-            foreach ($productImages as $productImage) {
-                $sourceImagePath = \PFAD_ROOT . \PFAD_MEDIA_IMAGE_STORAGE . $productImage->getCPath();
-                if (\file_exists($sourceImagePath)) {
-                    $image = self::getResizedArticleImage($sourceImagePath, 500, 500, 20);
-                    if ($imageNumber == 0) {
-                        \imagecopy($categoryImage, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
-                    } elseif ($imageNumber == 1) {
-                        \imagecopy($categoryImage, $image, 500, 24, 0, 0, imagesx($image), imagesy($image));
-                    } else {
-                        \imagecopy($categoryImage, $image, 250, 500 + 24, 0, 0, imagesx($image), imagesy($image));
-                    }
-
-                    \imagedestroy($image);
-                }
-
-                $imageNumber++;
-            }
-        } elseif ($productImagesCount == 2) {
-            $imageNumber = 0;
-            foreach ($productImages as $productImage) {
-                $sourceImagePath = \PFAD_ROOT . \PFAD_MEDIA_IMAGE_STORAGE . $productImage->getCPath();
-                if (\file_exists($sourceImagePath)) {
-                    $image = self::getResizedArticleImage($sourceImagePath, 500, 500);
-                    if ($imageNumber == 0) {
-                        \imagecopy($categoryImage, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
-                    } else {
-                        \imagecopy($categoryImage, $image, 500, 500, 0, 0, imagesx($image), imagesy($image));
-                    }
-
-                    \imagedestroy($image);
-                }
-
-                $imageNumber++;
-            }
-        } elseif ($productImagesCount == 1) {
-            $productImage = $productImages[0];
-
-            $sourceImagePath = \PFAD_ROOT . \PFAD_MEDIA_IMAGE_STORAGE . $productImage->getCPath();
-            if (\file_exists($sourceImagePath)) {
-                $image = self::getResizedArticleImage($sourceImagePath, 1024, 1024);
-                \imagecopy($categoryImage, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
-                \imagedestroy($image);
-            }
-
-        }
+        self::placeProductImageToCategoryImage($categoryImage, $productImages);
 
         $targetImageName = self::CATEGORY_IMAGE_NAME_PREFIX . $categoryId . '.png';
         $targetImagePath = \PFAD_ROOT . \STORAGE_CATEGORIES . $targetImageName;
@@ -104,62 +58,55 @@ class CategoryImageGenerator
     }
 
     /**
-     * @param int $width
-     * @param int $height
-     * @return false|\GdImage|resource
+     * @param $categoryImage
+     * @param Image[] $productImages
      */
-    private static function createTransparentImage(int $width, int $height)
+    private static function placeProductImageToCategoryImage($categoryImage, array $productImages)
     {
-        $image = \imagecreatetruecolor($width, $height);
-        $colorTransparent = \imagecolorallocatealpha($image, 0, 0, 0, 127);
-        \imagefill($image, 0, 0, $colorTransparent);
-        \imagealphablending($image, true);
-        \imagesavealpha($image, true);
-
-        return $image;
-    }
-
-
-    /**
-     * @param string $originalImagePath
-     * @param int $targetWidth
-     * @param int $targetHeight
-     * @param int $padding
-     * @return false|\GdImage|resource
-     */
-    private static function getResizedArticleImage(string $originalImagePath, int $targetWidth = 640, int $targetHeight = 640, int $padding = 15)
-    {
-        list($originalImageWidth, $originalImageHeight, $originalImageType) = \getimagesize($originalImagePath);
-        switch ($originalImageType) {
-            case \IMAGETYPE_GIF:
-                $imageOriginal = \imagecreatefromgif($originalImagePath);
-                break;
-            case \IMAGETYPE_PNG:
-                $imageOriginal = \imagecreatefrompng($originalImagePath);
-                break;
-            case \IMAGETYPE_JPEG:
-            default:
-                $imageOriginal = \imagecreatefromjpeg($originalImagePath);
-                break;
+        $productImageFiles = array();
+        foreach ($productImages as $productImage) {
+            $sourceImagePath = \PFAD_ROOT . \PFAD_MEDIA_IMAGE_STORAGE . $productImage->getCPath();
+            if (\file_exists($sourceImagePath)) {
+                $image = ImageUtils::createResizedImage($sourceImagePath, 1024, 1024, 40);
+                $productImageFiles[] = $image;
+            }
         }
 
-        if ($originalImageWidth > $originalImageHeight) {
-            $scale = $targetWidth / $originalImageWidth;
-        } else {
-            $scale = $targetHeight / $originalImageHeight;
+        $productImageFilesCount = sizeof($productImageFiles);
+        if ($productImageFilesCount == 3) {
+            $imageNumber = 0;
+            foreach ($productImageFiles as $productImageFile) {
+                if ($imageNumber == 0) {
+                    \imagecopyresized($categoryImage, $productImageFile, 0, 0, 0, 0, 500, 500, imagesx($productImageFile), imagesy($productImageFile));
+                } elseif ($imageNumber == 1) {
+                    \imagecopyresized($categoryImage, $productImageFile, 500, 24, 0, 0, 500, 500, imagesx($productImageFile), imagesy($productImageFile));
+                } else {
+                    \imagecopyresized($categoryImage, $productImageFile, 250, 500 + 24, 0, 0, 500, 500, imagesx($productImageFile), imagesy($productImageFile));
+                }
+
+                $imageNumber++;
+            }
+
+        } elseif ($productImageFilesCount == 2) {
+            $imageNumber = 0;
+            foreach ($productImageFiles as $productImageFile) {
+                if ($imageNumber == 0) {
+                    \imagecopyresized($categoryImage, $productImageFile, 0, 0, 0, 0, 500, 500, imagesx($productImageFile), imagesy($productImageFile));
+                } else {
+                    \imagecopyresized($categoryImage, $productImageFile, 500, 500, 0, 0, 500, 500, imagesx($productImageFile), imagesy($productImageFile));
+                }
+
+                $imageNumber++;
+            }
+        } elseif ($productImageFilesCount == 1) {
+            $productImageFile = $productImageFiles[0];
+
+            \imagecopyresized($categoryImage, $productImageFile, 0, 0, 0, 0, 1024, 1024, imagesx($productImageFile), imagesy($productImageFile));
         }
 
-        $newWidth = $originalImageWidth * $scale - $padding * 2;
-        $newHeight = $originalImageHeight * $scale - $padding * 2;
-
-        $offsetX = ($targetWidth - $newWidth) / 2;
-        $offsetY = ($targetHeight - $newHeight) / 2;
-
-        $imageResized = self::createTransparentImage($targetWidth, $targetHeight);
-        \imagecopyresampled($imageResized, $imageOriginal, $offsetX, $offsetY, 0, 0, $newWidth, $newHeight, $originalImageWidth, $originalImageHeight);
-        \imagedestroy($imageOriginal);
-
-        return $imageResized;
+        foreach ($productImageFiles as $productImageFile) {
+            \imagedestroy($productImageFile);
+        }
     }
 
 }
